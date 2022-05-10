4ccde5415970abe918a352f813d807fc0540155f
Gerson Calvo <gedacaba@gmail.com>
2022-05-10 15:05:30 -0500

    formulario listado de pacientes

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,36 @@
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: {""}
+        <span className="font-normal normal-case">Hook</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Propietario: {""}
+        <span className="font-normal normal-case">Gerson</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Email: {""}
+        <span className="font-normal normal-case">correo@correo.com</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Fecha alta: {""}
+        <span className="font-normal normal-case">10 mayo 2022 </span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        sintomas: {""}
+        <span className="font-normal normal-case">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s
+        </span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: {""}
+        <span className="font-normal normal-case">Hook</span>
+      </p>
+    </div>
+  );
+};
+
+export default Paciente;
