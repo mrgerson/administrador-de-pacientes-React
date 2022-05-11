7d13f8cd60dcf0f22524e14b0e89e6a8fa1cfec8
Gerson Calvo <gedacaba@gmail.com>
2022-05-11 11:53:10 -0500

    El prop {children}

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -57,7 +57,7 @@ function Formulario({ pacientes, setPacientes }) {
         onSubmit={handleSubmit}
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
       >
-        {error &&  <Error  mensaje='Todos los campos son obligatorios' /> }
+        {error &&  <Error><p>Todos los campos son obligatorios</p></Error> }
         <div className="mb-5">
           <label
             htmlFor="mascota"
